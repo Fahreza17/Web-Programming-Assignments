57cd9d1c8a848d94f8e7cb179ed848db92dcac01
Muhammad Aditya <91052274+tahubulatt@users.noreply.github.com>
2023-12-19 07:33:24 +0800

    Add files via upload

---
+++ H071221063/Final-Project/storage/framework/views/4b85ee4b5c48813a1faff0e018e94125.php
@@ -0,0 +1,68 @@
+
+<form method="POST" action="">
+    <?php echo csrf_field(); ?>
+    <div class="wrapper wrapper-login">
+		<div class="container container-login animated fadeIn">
+			<h3 class="text-center">RS IBU BAPAK</h3>
+			<div class="login-form">
+				<div class="form-group form-floating-label">
+					<input id="username" name="email" type="text" class="form-control input-border-bottom <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" autocomplete="off" autofocus required>
+                    <label for="username" class="placeholder">Username</label>
+                    <?php $__errorArgs = ['email'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                        <span class="invalid-feedback" role="alert">
+                            <strong>message</strong>
+                        </span>
+                     <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+				</div>
+				<div class="form-group form-floating-label">
+					<input id="password" name="password" type="password" class="form-control input-border-bottom <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?> is-invalid <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>" autocomplete="off" autofocus required>
+                    <label for="password" class="placeholder">Password</label>
+                    <?php $__errorArgs = ['password'];
+$__bag = $errors->getBag($__errorArgs[1] ?? 'default');
+if ($__bag->has($__errorArgs[0])) :
+if (isset($message)) { $__messageOriginal = $message; }
+$message = $__bag->first($__errorArgs[0]); ?>
+                        <span class="invalid-feedback" role="alert">
+                            <strong>message</strong>
+                        </span>
+                    <?php unset($message);
+if (isset($__messageOriginal)) { $message = $__messageOriginal; }
+endif;
+unset($__errorArgs, $__bag); ?>
+				</div>
+				<div class="row form-sub m-0">
+					<div class="custom-control custom-checkbox">
+						<input type="checkbox" class="custom-control-input" name="remember" id="remember" <?php echo e(old('remember') ? 'checked' : ''); ?>>
+						<label class="custom-control-label" for="remember">Remember Me</label>
+					</div>
+				</div>
+				<div class="form-action mb-3">
+					<button type="submit" class="btn btn-primary btn-rounded btn-login">Sign In</button>
+				</div>
+			</div>
+        </div>
+    </div>
+</form>
+
+<?php /**PATH C:\xampp\htdocs\Tugas-Final-Web-Prak\resources\views/homepage.blade.php ENDPATH**/ ?>
